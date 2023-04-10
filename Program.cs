9109f25beac2128a6e5f08499e9055877aa62d85
henrique <henriquebayer8@gmail.com>
2023-04-09 23:12:06 -0300

    feito parte da atividade extra

--- Program.cs
+++ Program.cs
@@ -1,231 +1,510 @@
-﻿Console.WriteLine($"Pesquisa sobre o produto");
+﻿// Console.WriteLine($"Pesquisa sobre o produto");
 
 
 
 
-Console.WriteLine($"Entrevistado 1");
+// Console.WriteLine($"Entrevistado 1");
 
-Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
-string S1 = Console.ReadLine();
+// Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
+// string S1 = Console.ReadLine();
 
-Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
+// Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
 
-Console.WriteLine($"Pergunta sobre o produto:");
-string R1 = Console.ReadLine();
+// Console.WriteLine($"Pergunta sobre o produto:");
+// string R1 = Console.ReadLine();
 
-Console.WriteLine($"Entrevistado 2");
+// Console.WriteLine($"Entrevistado 2");
 
-Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
-string S2 = Console.ReadLine();
+// Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
+// string S2 = Console.ReadLine();
 
-Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
+// Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
 
-Console.WriteLine($"Pergunta sobre o produto:");
-string R2 = Console.ReadLine();
+// Console.WriteLine($"Pergunta sobre o produto:");
+// string R2 = Console.ReadLine();
 
-Console.WriteLine($"Entrevistado 3");
+// Console.WriteLine($"Entrevistado 3");
 
-Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
-string S3 = Console.ReadLine();
+// Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
+// string S3 = Console.ReadLine();
 
-Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
+// Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
 
-Console.WriteLine($"Pergunta sobre o produto:");
-string R3 = Console.ReadLine();
+// Console.WriteLine($"Pergunta sobre o produto:");
+// string R3 = Console.ReadLine();
 
-Console.WriteLine($"Entrevistado 4");
+// Console.WriteLine($"Entrevistado 4");
 
-Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
-string S4 = Console.ReadLine();
+// Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
+// string S4 = Console.ReadLine();
 
-Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
+// Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
 
-Console.WriteLine($"Pergunta sobre o produto:");
-string R4 = Console.ReadLine();
+// Console.WriteLine($"Pergunta sobre o produto:");
+// string R4 = Console.ReadLine();
 
-Console.WriteLine($"Entrevistado 5");
+// Console.WriteLine($"Entrevistado 5");
 
-Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
-string S5 = Console.ReadLine();
+// Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
+// string S5 = Console.ReadLine();
 
-Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
+// Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
 
-Console.WriteLine($"Pergunta sobre o produto:");
-string R5 = Console.ReadLine();
+// Console.WriteLine($"Pergunta sobre o produto:");
+// string R5 = Console.ReadLine();
 
-Console.WriteLine($"Entrevistado 6");
+// Console.WriteLine($"Entrevistado 6");
 
-Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
-string S6 = Console.ReadLine();
+// Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
+// string S6 = Console.ReadLine();
 
-Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
+// Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
 
-Console.WriteLine($"Pergunta sobre o produto:");
-string R6 = Console.ReadLine();
+// Console.WriteLine($"Pergunta sobre o produto:");
+// string R6 = Console.ReadLine();
 
-Console.WriteLine($"Entrevistado 7");
+// Console.WriteLine($"Entrevistado 7");
 
-Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
-string S7 = Console.ReadLine();
+// Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
+// string S7 = Console.ReadLine();
 
-Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
+// Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
 
-Console.WriteLine($"Pergunta sobre o produto:");
-string R7 = Console.ReadLine();
+// Console.WriteLine($"Pergunta sobre o produto:");
+// string R7 = Console.ReadLine();
 
-Console.WriteLine($"Entrevistado 8");
+// Console.WriteLine($"Entrevistado 8");
 
-Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
-string S8 = Console.ReadLine();
+// Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
+// string S8 = Console.ReadLine();
 
-Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
+// Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
 
-Console.WriteLine($"Pergunta sobre o produto:");
-string R8 = Console.ReadLine();
+// Console.WriteLine($"Pergunta sobre o produto:");
+// string R8 = Console.ReadLine();
 
-Console.WriteLine($"Entrevistado 9");
+// Console.WriteLine($"Entrevistado 9");
 
-Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
-string S9 = Console.ReadLine();
+// Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
+// string S9 = Console.ReadLine();
 
-Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
+// Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
 
-Console.WriteLine($"Pergunta sobre o produto:");
-string R9 = Console.ReadLine();
+// Console.WriteLine($"Pergunta sobre o produto:");
+// string R9 = Console.ReadLine();
 
-Console.WriteLine($"Entrevistado 10");
+// Console.WriteLine($"Entrevistado 10");
 
-Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
-string S10 = Console.ReadLine();
+// Console.WriteLine($"Qual o seu sexo: m (masculino) ou f (feminino)");
+// string S10 = Console.ReadLine();
 
-Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
+// Console.WriteLine($"Responda a pergunta com s (sim) ou n (nao):");
 
-Console.WriteLine($"Pergunta sobre o produto:");
-string R10 = Console.ReadLine();
+// Console.WriteLine($"Pergunta sobre o produto:");
+// string R10 = Console.ReadLine();
 
-int RValues = 0;
-int RValuen = 0;
-
-
-
-if(R1 == "s")
-{RValues += 1;
-}
-if(R2 == "s")
-{RValues += 1;
-}
-if(R3 == "s")
-{RValues += 1;
-}
-if(R4 == "s")
-{RValues += 1;
-}
-if(R5 == "s")
-{RValues += 1;
-}
-if(R6 == "s")
-{RValues += 1;
-}
-if(R7 == "s")
-{RValues += 1;
-}
-if(R8 == "s")
-{RValues += 1;
-}
-if(R9 == "s")
-{RValues += 1;
-}
-if(R10 == "s")
-{RValues += 1;
-}
+// int RValues = 0;
+// int RValuen = 0;
 
 
-if(R1 == "n")
-{RValuen += 1;
-}
-if(R2 == "n")
-{RValuen += 1;
-}
-if(R3 == "n")
-{RValuen += 1;
-}
-if(R4 == "n")
-{RValuen += 1;
-}
-if(R5 == "n")
-{RValuen += 1;
-}
-if(R6 == "n")
-{RValuen += 1;
-}
-if(R7 == "n")
-{RValuen += 1;
-}
-if(R8 == "n")
-{RValuen += 1;
-}
-if(R9 == "n")
-{RValuen += 1;
-}
-if(R10 == "n")
-{RValuen += 1;
-}
 
-Console.WriteLine($"a quantidade de pessoas que respondeu sim foi de {RValues}");
-Console.WriteLine($"a quantidade de pessoas que respondeu nao foi de {RValuen}");
-
-// CONTADOR FEMININO QUE RESPONDEU SIM
-int Contfems = 0;
-
-if(S1 == "f" && R1 == "s")
-{Contfems += 1;}
-if(S2 == "f" && R2 == "s")
-{Contfems += 1;}
-if(S3 == "f" && R3 == "s")
-{Contfems += 1;}
-if(S4 == "f" && R4 == "s")
-{Contfems += 1;}
-if(S5 == "f" && R5 == "s")
-{Contfems += 1;}
-if(S6 == "f" && R6 == "s")
-{Contfems += 1;}
-if(S7 == "f" && R7 == "s")
-{Contfems += 1;}
-if(S8 == "f" && R8 == "s")
-{Contfems += 1;}
-if(S9 == "f" && R9 == "s")
-{Contfems += 1;}
-if(S10 == "f" && R10 == "s")
-{Contfems += 1;}
-
-Console.WriteLine($"A quantidade de mulheres que respondeu sim foi de {Contfems}");
+// if(R1 == "s")
+// {RValues += 1;
+// }
+// if(R2 == "s")
+// {RValues += 1;
+// }
+// if(R3 == "s")
+// {RValues += 1;
+// }
+// if(R4 == "s")
+// {RValues += 1;
+// }
+// if(R5 == "s")
+// {RValues += 1;
+// }
+// if(R6 == "s")
+// {RValues += 1;
+// }
+// if(R7 == "s")
+// {RValues += 1;
+// }
+// if(R8 == "s")
+// {RValues += 1;
+// }
+// if(R9 == "s")
+// {RValues += 1;
+// }
+// if(R10 == "s")
+// {RValues += 1;
+// }
 
 
-// CONTAGEM EM PORCENTAGEM DE HOMENS QUE RESPONDERAM NAO
+// if(R1 == "n")
+// {RValuen += 1;
+// }
+// if(R2 == "n")
+// {RValuen += 1;
+// }
+// if(R3 == "n")
+// {RValuen += 1;
+// }
+// if(R4 == "n")
+// {RValuen += 1;
+// }
+// if(R5 == "n")
+// {RValuen += 1;
+// }
+// if(R6 == "n")
+// {RValuen += 1;
+// }
+// if(R7 == "n")
+// {RValuen += 1;
+// }
+// if(R8 == "n")
+// {RValuen += 1;
+// }
+// if(R9 == "n")
+// {RValuen += 1;
+// }
+// if(R10 == "n")
+// {RValuen += 1;
+// }
 
-int mValueCalc = 0;
+// Console.WriteLine($"a quantidade de pessoas que respondeu sim foi de {RValues}");
+// Console.WriteLine($"a quantidade de pessoas que respondeu nao foi de {RValuen}");
 
-if(S1 == "m" && R1 == "n")
-{mValueCalc += 1;}
-if(S2 == "m" && R2 == "n")
-{mValueCalc += 1;}
-if(S3 == "m" && R3 == "n")
-{mValueCalc += 1;}
-if(S4 == "m" && R4 == "n")
-{mValueCalc += 1;}
-if(S5 == "m" && R5 == "n")
-{mValueCalc += 1;}
-if(S6 == "m" && R6 == "n")
-{mValueCalc += 1;}
-if(S7 == "m" && R7 == "n")
-{mValueCalc += 1;}
-if(S8 == "m" && R8 == "n")
-{mValueCalc += 1;}
-if(S9 == "m" && R9 == "n")
-{mValueCalc += 1;}
-if(S10 == "m" && R10 == "n")
-{mValueCalc += 1;}
+// // CONTADOR FEMININO QUE RESPONDEU SIM
+// int Contfems = 0;
 
-double porcentagem = (mValueCalc * 100) / 10;
+// if(S1 == "f" && R1 == "s")
+// {Contfems += 1;}
+// if(S2 == "f" && R2 == "s")
+// {Contfems += 1;}
+// if(S3 == "f" && R3 == "s")
+// {Contfems += 1;}
+// if(S4 == "f" && R4 == "s")
+// {Contfems += 1;}
+// if(S5 == "f" && R5 == "s")
+// {Contfems += 1;}
+// if(S6 == "f" && R6 == "s")
+// {Contfems += 1;}
+// if(S7 == "f" && R7 == "s")
+// {Contfems += 1;}
+// if(S8 == "f" && R8 == "s")
+// {Contfems += 1;}
+// if(S9 == "f" && R9 == "s")
+// {Contfems += 1;}
+// if(S10 == "f" && R10 == "s")
+// {Contfems += 1;}
 
-Console.WriteLine($"A porcentagem de homens que responderam nao e de: {porcentagem}%");
+// Console.WriteLine($"A quantidade de mulheres que respondeu sim foi de {Contfems}");
+
+
+// // CONTAGEM EM PORCENTAGEM DE HOMENS QUE RESPONDERAM NAO
+
+// int mValueCalc = 0;
+
+// if(S1 == "m" && R1 == "n")
+// {mValueCalc += 1;}
+// if(S2 == "m" && R2 == "n")
+// {mValueCalc += 1;}
+// if(S3 == "m" && R3 == "n")
+// {mValueCalc += 1;}
+// if(S4 == "m" && R4 == "n")
+// {mValueCalc += 1;}
+// if(S5 == "m" && R5 == "n")
+// {mValueCalc += 1;}
+// if(S6 == "m" && R6 == "n")
+// {mValueCalc += 1;}
+// if(S7 == "m" && R7 == "n")
+// {mValueCalc += 1;}
+// if(S8 == "m" && R8 == "n")
+// {mValueCalc += 1;}
+// if(S9 == "m" && R9 == "n")
+// {mValueCalc += 1;}
+// if(S10 == "m" && R10 == "n")
+// {mValueCalc += 1;}
+
+// double porcentagem = (mValueCalc * 100) / 10;
+
+// Console.WriteLine($"A porcentagem de homens que responderam nao e de: {porcentagem}%");
+
+
+
+// EXERCICIO 2 
+
+Console.WriteLine($"Pessoa N°1");
+
+
+Console.WriteLine($"Informe o sua idade:");
+int idade1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu peso:");
+float peso1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu sexo: m (masculino) f (feminino)");
+char sexo1 = char.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Pessoa N°2");
+
+
+Console.WriteLine($"Informe o sua idade:");
+int idade2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu peso:");
+float peso2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu sexo: m (masculino) f (feminino)");
+char sexo2 = char.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Pessoa N°3");
+
+
+Console.WriteLine($"Informe o sua idade:");
+int idade3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu peso:");
+float peso3 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu sexo: m (masculino) f (feminino)");
+char sexo3 = char.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Pessoa N°4");
+
+
+Console.WriteLine($"Informe o sua idade:");
+int idade4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu peso:");
+float peso4 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu sexo: m (masculino) f (feminino)");
+char sexo4 = char.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Pessoa N°5");
+
+
+Console.WriteLine($"Informe o sua idade:");
+int idade5 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu peso:");
+float peso5 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu sexo: m (masculino) f (feminino)");
+char sexo5 = char.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Pessoa N°6");
+
+
+Console.WriteLine($"Informe o sua idade:");
+int idade6 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu peso:");
+float peso6 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu sexo: m (masculino) f (feminino)");
+char sexo6 = char.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Pessoa N°7");
+
+
+Console.WriteLine($"Informe o sua idade:");
+int idade7 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu peso:");
+float peso7 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu sexo: m (masculino) f (feminino)");
+char sexo7 = char.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Pessoa N°8");
+
+
+Console.WriteLine($"Informe o sua idade:");
+int idade8 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu peso:");
+float peso8 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu sexo: m (masculino) f (feminino)");
+char sexo8 = char.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Pessoa N°9");
+
+
+Console.WriteLine($"Informe o sua idade:");
+int idade9 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu peso:");
+float peso9 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu sexo: m (masculino) f (feminino)");
+char sexo9 = char.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"Pessoa N°10");
+
+
+Console.WriteLine($"Informe o sua idade:");
+int idade10 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu peso:");
+float peso10 = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Informe o seu sexo: m (masculino) f (feminino)");
+char sexo10 = char.Parse(Console.ReadLine());
+
+
+// CONTADOR DE HOMENS
+
+int homens = 0;
+
+if (sexo1 == 'm')
+{homens += 1;
+    
+    }
+
+
+if (sexo2 == 'm')
+{homens += 1;
+    
+    }
+
+
+if (sexo3 == 'm')
+{homens += 1;
+    
+    }
+
+
+if (sexo4 == 'm')
+{homens += 1;
+    
+    }
+
+
+if (sexo5 == 'm')
+{homens += 1;
+    
+    }
+
+
+if (sexo6 == 'm')
+{homens += 1;
+    
+    }
+
+
+if (sexo7 == 'm')
+{homens += 1;
+    
+    }
+
+
+if (sexo8 == 'm')
+{homens += 1;
+    
+    }
+
+
+if (sexo9 == 'm')
+{homens += 1;
+    
+    }
+
+
+if (sexo10 == 'm')
+{homens += 1;
+    
+    }
+
+Console.WriteLine($"O total de homens é de {homens}");
+
+
+// CONTADOR DE MULHERES 
+
+int mulheres = 0;
+
+if (sexo1 == 'f')
+{mulheres += 1;
+    
+    }
+
+
+if (sexo2 == 'f')
+{mulheres += 1;
+    
+    }
+
+
+if (sexo3 == 'f')
+{mulheres += 1;
+    
+    }
+
+
+if (sexo4 == 'f')
+{mulheres += 1;
+    
+    }
+
+
+if (sexo5 == 'f')
+{mulheres += 1;
+    
+    }
+
+
+if (sexo6 == 'f')
+{mulheres += 1;
+    
+    }
+
+
+if (sexo7 == 'f')
+{mulheres += 1;
+    
+    }
+
+
+if (sexo8 == 'f')
+{mulheres += 1;
+    
+    }
+
+
+if (sexo9 == 'f')
+{mulheres += 1;
+    
+    }
+
+
+if (sexo10 == 'f')
+{mulheres += 1;
+    
+    }
+
+Console.WriteLine($"O total de mulheres é de {mulheres}");
+
+
+// MÉDIA DE IDADE DOS HOMENS E MULHERES
+
+Double mediah = (idade1 + idade2 + idade3 + idade4 + idade5 + idade6 + idade7 + idade8 + idade9 + idade10) / 2;
+
+Console.WriteLine($"A média de idade dos homens é de {mediah}");
+
+
+
+
+
+
+
